e5b7013f7d73e38dbf872b361f319f766ff08ebd
Cazaks <calebezak@gmail.com>
2025-07-06 02:09:49 +0100

    adding how it can also be done

---
+++ PrintOneToFourOnTheSameLine.java
@@ -0,0 +1,19 @@
+public class PrintOneToFourOnTheSameLine{
+	public static void main(String[]args){
+	
+	System.out.print("1, 2, ");
+	System.out.printf("3, ");
+	System.out.println("4");
+
+	System.out.println("It can also be done this way");
+
+	System.out.println("1, 2, 3, 4");
+	System.out.print("1, 2, 3, 4\n");
+	System.out.println("1, 2, 3, 4");
+	
+	
+
+
+		
+	}
+}
