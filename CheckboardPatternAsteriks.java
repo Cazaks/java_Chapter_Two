1fae61fd278a8c9db9fba716d053028ddcece813
Cazaks <calebezak@gmail.com>
2025-07-07 02:01:10 +0100

    Checkboard pattern exercise

---
+++ CheckboardPatternAsteriks.java
@@ -0,0 +1,10 @@
+public class CheckboardPatternAsteriks{
+public static void main(String[] args){
+
+ System.out.printf("* * * * * * * *%n * * * * * * * *%n* * * * * * * *%n * * * * * * * *%n* * * * * * * *%n * * * * * * * *%n* * * * * * * *%n * * * * * * * *%n");
+
+//System.out.printf("*********%n*       *%n* * * * * * * *%n * * * * * * * *%n* * * * * * * *%n * * * * * * * *%n* * * * * * * *%n * * * * * * * *%n");
+
+
+ }
+}
