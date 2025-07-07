b2c6f90b4fcc371d23aeddd1b63f4bace93dad74
Cazaks <calebezak@gmail.com>
2025-07-07 11:28:19 +0100

    Table of square and cube exercise

---
+++ TableOfSquareAndCube.java
@@ -0,0 +1,34 @@
+public class TableOfSquareAndCube {
+	public static void main(String[] args){
+
+		System.out.println("number\tsquare\tcube\n");
+		
+		System.out.println("0\t" + 0 * 0 + "\t" + 0 * 0 * 0 + "\n");
+
+		System.out.println("1\t" + 1 * 1 + "\t" + 1 * 1 * 1 + "\n");
+
+		System.out.println("2\t" + 2 * 2 + "\t" + 2 * 2 * 2 + "\n");
+
+		System.out.println("3\t" + 3 * 3 + "\t" + 3 * 3 * 3 + "\n");
+
+		System.out.println("4\t" + 4 * 4 + "\t" + 4 * 4 * 4 + "\n");
+
+		System.out.println("5\t" + 5 * 5 + "\t" + 5 * 5 * 5 + "\n");
+
+		System.out.println("6\t" + 6 * 6 + "\t" + 6 * 6 * 6 + "\n");
+
+		System.out.println("7\t" + 7 * 7 + "\t" + 7 * 7 * 7 + "\n");
+
+		System.out.println("8\t" + 8 * 8 + "\t" + 8 * 8 * 8 + "\n");
+
+		System.out.println("9\t" + 9 * 9 + "\t" + 9 * 9 * 9 + "\n");
+
+		System.out.println("10\t" + 10 * 10 + "\t" + 10 * 10 * 10 + "\n");
+
+		//System.out.println("5\t" + 5 * 5 + "\t" + 5 * 5 * 5 + "\n");
+
+
+
+		
+	}	
+}
